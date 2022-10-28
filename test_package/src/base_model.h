2121853ece03e4c3afb708135754b7b8c6786f84
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2022-10-28 16:04:37 +0200

    agentlist as vector of shared_ptr

--- test_package/src/base_model.h
+++ test_package/src/base_model.h
@@ -1,14 +1,14 @@
 #include <catch2/catch_all.hpp>
 
-#include <iostream>
+#include <memory>
 
 #include "Dyno.h"
 
 TEST_CASE( "Base Model", "[base]" ) {
     Dyno::Model m = Dyno::Model{ };
-    Dyno::Agent* a = m.add_agent<Dyno::Agent>();
+    std::weak_ptr<Dyno::Agent> a = m.add_agent<Dyno::Agent>();
 
-    std::cout << a -> get_id() << std::endl;
-    std::cout << m.get_n_of_agents() << std::endl;
+    REQUIRE(a.lock() -> get_id() == 1);
+    REQUIRE(m.get_n_of_agents() == 1);
 
 }
